909efe4e07ea1ffc0841fc69f163fd6b230a1719
Annice Mathew <annsays1@gmail.com>
2017-03-31 11:54:09 -0700

    Initial commit

---
+++ db.js
@@ -0,0 +1,39 @@
+// db.js
+module.exports = function() {
+  return {
+    politics0: require('./crawler/politics0.json'),
+    politics1: require('./crawler/politics1.json'),
+	politics2: require('./crawler/politics2.json'),
+    politics3: require('./crawler/politics3.json'),
+	politics4: require('./crawler/politics4.json'),
+	travel0: require('./crawler/travel0.json'),
+    travel1: require('./crawler/travel1.json'),
+	travel2: require('./crawler/travel2.json'),
+    travel3: require('./crawler/travel3.json'),
+	opinion0: require('./crawler/opinion0.json'),
+	opinion1: require('./crawler/opinion1.json'),
+	opinion2: require('./crawler/opinion2.json'),
+	opinion3: require('./crawler/opinion3.json'),
+	opinion4: require('./crawler/opinion4.json'),
+	opinion5: require('./crawler/opinion5.json'),
+	health0: require('./crawler/health0.json'),
+	health1: require('./crawler/health1.json'),
+	health2: require('./crawler/health2.json'),
+	health3: require('./crawler/health3.json'),
+	health4: require('./crawler/health4.json'),
+	faithreligion0: require('./crawler/faithreligion0.json'),
+	faithreligion1: require('./crawler/faithreligion1.json'),
+	faithreligion2: require('./crawler/faithreligion2.json'),
+	faithreligion3: require('./crawler/faithreligion3.json'),
+	faithreligion4: require('./crawler/faithreligion4.json'),
+	entertainment0: require('./crawler/entertainment0.json'),
+	entertainment1: require('./crawler/entertainment1.json'),
+	entertainment2: require('./crawler/entertainment2.json'),
+	entertainment3: require('./crawler/entertainment3.json')
+
+
+	
+	
+	
+  }
+}
